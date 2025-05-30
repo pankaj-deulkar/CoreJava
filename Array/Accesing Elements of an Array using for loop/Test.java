6997dab1e0272b30aa490a95846c24a3117327c5
pankaj-deulkar <149669430+pankaj-deulkar@users.noreply.github.com>
2025-05-30 14:07:14 +0530

    Update Test.java

--- Array/Accesing Elements of an Array using for loop/Test.java
+++ Array/Accesing Elements of an Array using for loop/Test.java
@@ -2,7 +2,7 @@ public class Test{
 	
 	public static void main(String[] args)
 	{
-		String[] s={"A","B","C","D","E","F","G"};
+		String[] s={"A","B","C","D","E","F","G","H"};
 		
 		for(int i=0;i<s.length;i++)
 		{
